8003fb969fd5b8f3d0441498a1971088e683d776
Tikava <wavoleniu@gmail.com>
2023-12-24 00:07:16 +0600

    Update .storybook

--- assignment-15/.storybook/main.js
+++ assignment-15/.storybook/main.js
@@ -1,6 +1,9 @@
 /** @type { import('@storybook/vue3-webpack5').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../src/stories/**/*.mdx", 
+    "../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
